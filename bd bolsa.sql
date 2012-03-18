8e55f8896b3008bb5624d4958b9a41c8ed850ac6
Paulo Cesar Soares De Brito <pcego36@gmail.com>
2012-03-18 12:25:14 -0300

    adicionado view precomedio obtem preco medio das ofertas para uma acao em um determinado dia

--- bd bolsa.sql
+++ bd bolsa.sql
@@ -13,6 +13,10 @@ primary key(codigo),
 
 create index Ixcpf on usuarios(cpf);
 
+insert into usuarios(nome,cpf) values ('cesar','951.112.126-04')
+insert into usuarios(nome,cpf) values ('paulo cesar','951.112.125-04')
+insert into usuarios(nome,cpf) values ('jose cesar','951.112.124-04')
+
 create table acoes(
 
 codigo int identity(1,1) not null,
@@ -24,6 +28,10 @@ primary key(codigo),
 
 create index Ixtipo on acoes(tipo);
 create index Ixnome on acoes(nome);
+
+insert into acoes(tipo,nome) values('preferencial','petr01')
+insert into acoes(tipo,nome) values('ordinaria','vale05')
+insert into acoes(tipo,nome) values('preferencial','milk01')
 
 create table acoesxusuarios(
 
@@ -39,6 +47,10 @@ foreign key (acao) references acoes(codigo),
 );
 
 create index Ixdata on acoesxusuarios(data);
+
+insert into acoesxusuarios(usuario,acao,data,quant) values(1,1,'2012/03/18',1000)
+insert into acoesxusuarios(usuario,acao,data,quant) values(2,2,'2012/03/18',2000)
+insert into acoesxusuarios(usuario,acao,data,quant) values(3,3,'2012/03/18',3000)
 
 create table transacoes(
 
@@ -59,11 +71,33 @@ foreign key(usuario) references usuarios(codigo),
 
 create index Ixdata on transacoes(data);
 
+insert into transacoes(acao,usuario,situacao,data,quant,tipo,valor_unitario) values(1,1,0,'2012/03/10',100,0,1.70)
+insert into transacoes(acao,usuario,situacao,data,quant,tipo,valor_unitario) values(2,2,0,'2012/03/11',150,0,1.78)
+insert into transacoes(acao,usuario,situacao,data,quant,tipo,valor_unitario) values(3,3,0,'2012/03/09',105,0,1.79)
+
+insert into transacoes(acao,usuario,origem,situacao,data,quant,tipo,valor_unitario) values(2,1,1,0,'2012/03/18',10,1,1.99)
+insert into transacoes(acao,usuario,origem,situacao,data,quant,tipo,valor_unitario) values(1,2,2,0,'2012/03/18',100,1,2.85)
+insert into transacoes(acao,usuario,origem,situacao,data,quant,tipo,valor_unitario) values(2,3,3,0,'2012/03/18',120,1,2.88)
+
+insert into transacoes(acao,usuario,origem,situacao,data,quant,tipo,valor_unitario) values(1,1,1,1,'2012/03/10',10,1,1.60)
+insert into transacoes(acao,usuario,origem,situacao,data,quant,tipo,valor_unitario) values(2,2,2,1,'2012/03/11',10,1,1.90)
+insert into transacoes(acao,usuario,origem,situacao,data,quant,tipo,valor_unitario) values(3,3,3,1,'2012/03/13',10,1,1.80)
+
 create view precomedio as
-select a.nome as acao,AVG(t.valor_unitario) as media
+select t.data as data,a.nome as acao,AVG(t.valor_unitario) as media
 from transacoes as t join acoes as a on t.acao = a.codigo
-where t.tipo = 1 and t.data = GETDATE()
-group by a.nome
+where t.tipo = 1
+group by a.nome, t.data
+
+
+select *from precomedio where data = '2012/03/18'
+
+
+
+
+
+
+
 
 
 
